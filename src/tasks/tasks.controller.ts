51e559b09d1a654b6f40239426a1814bc6a6d2b6
Endiaz <endiaz.arsanto@equity.id>
2024-09-14 20:59:40 +0700

    commit

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { TaskModel } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {
+  getAllTasks(): TaskModel[] {
     return this.tasksService.getAllTasks();
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { TaskModel, TaskStatus } from './task.model';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: TaskModel[] = [];
 
-  getAllTasks() {
+  getAllTasks(): TaskModel[] {
     return this.tasks;
+  }
+
+  createTask(title: string, description: string): TaskModel {
+    const task: TaskModel = {
+      id: uuidv4(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
   }
 }
