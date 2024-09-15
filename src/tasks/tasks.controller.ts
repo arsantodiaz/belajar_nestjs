d022ee100a7eafb835571e700a2dd1e5d6a8f1d1
Endiaz <endiaz.arsanto@equity.id>
2024-09-15 10:00:07 +0700

    commit

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TaskModel } from './task.model';
 
@@ -6,8 +6,33 @@ import { TaskModel } from './task.model';
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
+  /**
+   * Retrieves all tasks.
+   *
+   * @returns {TaskModel[]} An array of all tasks.
+   */
   @Get()
   getAllTasks(): TaskModel[] {
     return this.tasksService.getAllTasks();
+  }
+
+  // @Post()
+  // createTask(@Body() body) {
+  //   console.log('body', body);
+  // }
+
+  /**
+   * Post a task.
+   *
+   * @returns {TaskModel[]} An array of all tasks.
+   */
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): TaskModel {
+    console.log('title', title);
+    console.log('description', description);
+    return this.tasksService.createTask(title, description);
   }
 }
